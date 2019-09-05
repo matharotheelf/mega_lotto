3ea01bea916a664ce3827629d8f487e8ef9fc453
matharo <thomas.lawrence@homebrew.co.uk>
2019-09-05 16:11:45 +0100

    Added rspec and pry for debugging

---
+++ lib/mega_lotto.rb
@@ -0,0 +1,10 @@
+require "mega_lotto/version"
+begin
+  require "pry"
+rescue LoadError
+end
+
+module MegaLotto
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/mega_lotto_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe MegaLotto do
+  it "has a version number" do
+    expect(MegaLotto::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
